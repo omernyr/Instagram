f4f8f6b5c5446dd1cf36e4f75a450620a022526c
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-09 20:38:36 +0300

    Upgrade loginVC

--- InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,9 +9,68 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    struct Constants {
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
+    private let userNameTextField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username or Email..."
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        return field
+    }()
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let privacyButtpn: UIButton = {
+        return UIButton()
+    }()
+    
+    private let termsButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let createAccountButton: UIButton = {
+        return UIButton()
+    }()
+    
+    private let headerView: UIView = {
+        return UIView()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addSubviews()
         view.backgroundColor = .red
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(userNameTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(termsButton)
+        view.addSubview(privacyButtpn)
+    }
+    
+    @objc private func didTapLoginButton() {}
+    @objc private func didTapTermsButton() {}
+    @objc private func didTapPrivacyButton() {}
+    @objc private func didTapCreateAccountButton() {}
 }
