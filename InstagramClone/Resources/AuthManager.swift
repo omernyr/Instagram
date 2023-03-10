568f92bc2b42294dbb38357d6f22ae6b05b14274
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-10 23:46:42 +0300

    Uprade Firebase Manager

--- InstagramClone/Resources/AuthManager.swift
+++ InstagramClone/Resources/AuthManager.swift
@@ -10,8 +10,37 @@ import FirebaseAuth
 public class AuthManager {
     static let shared = AuthManager()
     
-    public func registerNewUser(username: String, email: String, password: String) {
+    public func registerNewUser(username: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        /*
+         Check if username is avaiabla
+         Check if email is avaiabla
+         Create account
+         Insert account to database
+         */
         
+        DatabaseManager.shared.canCreateNewUser(with: email, username: username) { canCreate in
+            
+            if canCreate {
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    guard error == nil, result != nil else {
+                        completion(false)
+                        return
+                    }
+                    DatabaseManager.shared.insertNewUser(with: email, username: username) { inserted in
+                        if inserted {
+                            completion(true)
+                            return
+                        } else {
+                            // failed to insert databse
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            } else {
+                completion(false)
+            }
+        }
     }
     
     public func loginUser(username: String?, email: String?, password: String, completion: @escaping (Bool) -> Void) {
--- InstagramClone/Resources/DatabaseManager.swift
+++ InstagramClone/Resources/DatabaseManager.swift
@@ -10,15 +10,26 @@ import FirebaseDatabase
 public class DatabaseManager {
     static let shared = DatabaseManager()
     
+    private let database = Database.database().reference()
+    
     //MARK: Public
     
-    public func registerNewUser(username: String, email: String, password: String) {
-        
+    public func canCreateNewUser(with email: String, username: String, completion: (Bool) -> Void) {
+        completion(true)
     }
     
-    public func loginUser(username: String, email: String, password: String) {
-        
+    public func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        let key = email.safeDatabaseKey()
+        database.child(key).setValue(["username": username]) { error, _ in
+            if error == nil {
+                // successed
+                completion(true)
+                return
+            } else {
+                // failed
+                completion(false)
+                return
+            }
+        }
     }
-    
-    
 }
