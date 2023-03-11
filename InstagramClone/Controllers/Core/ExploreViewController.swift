55202d7ed605839691d5a0cb5d3dcb150a5054d3
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-11 18:00:10 +0300

    added exploreVc

--- InstagramClone/Controllers/Core/ExploreViewController.swift
+++ InstagramClone/Controllers/Core/ExploreViewController.swift
@@ -8,22 +8,50 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .secondarySystemBackground
+        return searchBar
+    }()
+    
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.navigationBar.topItem?.titleView = searchBar
         view.backgroundColor = .systemBackground
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 90, height: 90) 
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView?.dataSource = self
+        collectionView?.delegate = self
+        
+        guard let collectionView = collectionView else { return }
+        view.addSubview(collectionView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
     }
-    */
+    
+}
 
+extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.systemGreen.cgColor
+        return cell
+    }
+    
 }
