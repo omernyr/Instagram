790f17aa15d82e44326864771a8939ad5c8ba0cd
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-13 16:32:09 +0300

    Added tableView in homeVC

--- InstagramClone/Controllers/Core/HomeViewController.swift
+++ InstagramClone/Controllers/Core/HomeViewController.swift
@@ -10,18 +10,28 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(IGFeedPostTableViewCell.self, forCellReuseIdentifier: IGFeedPostTableViewCell.identifier)
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .systemBackground
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
         /// Check auth status
         handleNotAuthenticated()
-        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
     
     private func handleNotAuthenticated() {
@@ -31,5 +41,20 @@ class HomeViewController: UIViewController {
             loginVC.modalPresentationStyle = .fullScreen
             present(loginVC, animated: true)
         }
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell                 
+        return UITableViewCell()
     }
 }
