52fb1bd76dea3c1c796b71ab456d6299338bc5e7
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-15 11:50:48 +0300

    added PhotoCollectionViewCell

---
+++ InstagramClone/Views/Cells/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  InstagramClone
+//
+//  Created by macbook pro on 15.03.2023.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "PhotoCollectionViewCell"
+    
+}
