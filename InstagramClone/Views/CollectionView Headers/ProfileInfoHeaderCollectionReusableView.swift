278246f9284b385fd7d6ab23ca0aa496d4ed8de0
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-15 11:51:07 +0300

    added ProfileInfoHeaderCollectionReusableView

---
+++ InstagramClone/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  InstagramClone
+//
+//  Created by macbook pro on 15.03.2023.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
+    
+}
