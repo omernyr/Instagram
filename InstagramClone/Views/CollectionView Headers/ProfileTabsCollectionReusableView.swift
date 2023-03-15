7fbc4f7e7aaea367ecd3f3586cba0b69a23d834f
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-03-15 11:51:20 +0300

    added ProfileTabsCollectionReusableView

---
+++ InstagramClone/Views/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileTabsCollectionReusableView.swift
+//  InstagramClone
+//
+//  Created by macbook pro on 15.03.2023.
+//
+
+import UIKit
+
+class ProfileTabsCollectionReusableView: UICollectionReusableView {
+      
+    static let identifier = "ProfileTabsCollectionReusableView"
+}
